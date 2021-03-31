c37350527698c52492a447ca096bb158d08a1d5c
Karin Dahlstedt <dahlstedt.karin@gmail.com>
2021-03-31 18:12:39 +0200

    hamburger menu, test 2

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,7 @@
-console.log($('h1'));
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
