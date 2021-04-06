d8c9d434f648777fb2983eaeabc4f43f8248153e
Karin Dahlstedt <dahlstedt.karin@gmail.com>
2021-04-06 18:51:34 +0200

    Scrolling process bar added

--- assets/app.js
+++ assets/app.js
@@ -5,3 +5,18 @@ function openNav() {
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
+
+  
+  $(document).on("scroll", function(){
+    var pixels = $(document).scrollTop();
+    var pageHeight = $(document).height() - $(window).height();
+    var progress = 100 * pixels / pageHeight;
+    
+    $("div.progress").css("width", progress + "%");
+  })
+  
+  // ---- Notes ---- //
+  
+  // pixels = amount of px from top of page
+  // pageHeight = whole page height minus viewable page height
+  // progress = pixels from top / page-height * 100
